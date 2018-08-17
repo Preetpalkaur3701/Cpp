d83627cce79db4c15d3bd4885d5e09f476ffe169
Preetpal Kaur <preetpalok123@gmail.com>
2018-08-17 09:26:39 +0500

    Armstrong number

---
+++ armstrong.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+    int x,y,z,answer;
+    cout<<"Enter first number";
+    cin>> x;
+    cout<< "Enter second number";
+    cin>>y;
+    cout<< "Enter the third number";
+    cin>>z;
+    answer = x*x*x + y*y*y + z*z*z;
+    cout<< answer;
+}
